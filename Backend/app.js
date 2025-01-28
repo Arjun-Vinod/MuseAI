3b417f34c5f59341b63ac2cf6a39d6cb2ea5acfc
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-01-28 12:25:31 +0530

    Add controllers,middlewares,routes,update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,19 @@ const app=express();
 require('dotenv').config();
 const connectDB=require('./config/db');
 connectDB();
+const authRoutes=require('./routes/auth');
+const cors=require("cors");
+
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth',authRoutes)
+
+app.get('/test', (req, res) => {
+    res.send('Server is working');
+});
+
+
 const PORT=4000;
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`)
